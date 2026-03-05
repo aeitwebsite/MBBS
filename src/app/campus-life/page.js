b070617646b199d9f7316ce151c55cd062da57e1
lavanyamoger <lavanyamoger183@gmail.com>
2026-03-05 10:28:22 +0530

    Update: Minor error in /campus-life fixed

--- src/app/campus-life/page.js
+++ src/app/campus-life/page.js
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import Campuslife from "@/components/Campuslife";
 
 export default function CampusLifePage() {
-  return <Campuslife />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Campuslife />
+    </Suspense>
+  );
 }
